21ce32e504faac0685638eb81099a7fc5df50358
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-05-02 07:21:39 +0200

    addTask Js shorter

--- js/add_task.js
+++ js/add_task.js
@@ -1,6 +1,4 @@
-function togglePriority(priority) {
-  let elements = document.getElementsByClassName('prioBtn');
-
+function resetElements(elements) {
   for (let i = 0; i < elements.length; i++) {
     elements[i].classList.remove('selected');
     elements[i].style.backgroundColor = 'white';
@@ -17,39 +15,34 @@ function togglePriority(priority) {
       }
     }
   }
+}
+
+function setPriorityStyles(selectedElement, backgroundColor, textColor, svgColor) {
+  if (selectedElement) {
+    selectedElement.classList.add('selected');
+    selectedElement.style.backgroundColor = backgroundColor;
+    selectedElement.style.color = textColor;
+    let svgPaths = selectedElement.querySelectorAll('svg path');
+    if (svgPaths) {
+      for (let j = 0; j < svgPaths.length; j++) {
+        svgPaths[j].style.fill = svgColor;
+      }
+    }
+  }
+}
+
+function togglePriority(priority) {
+  let elements = document.getElementsByClassName('prioBtn');
+  resetElements(elements);
 
   // Hintergrundfarbe, Textfarbe und SVG-Farbe basierend auf der ausgewählten Priorität festlegen
   let selectedElement = document.querySelector("[onclick*='" + priority + "']");
-  if (selectedElement) {
-    selectedElement.classList.add('selected');
-    if (priority === 'urgent') {
-      selectedElement.style.backgroundColor = '#FF3D00'; // Rot
-      selectedElement.style.color = 'white'; // Weiße Textfarbe
-      let svgPaths = selectedElement.querySelectorAll('svg path');
-      if (svgPaths) {
-        for (let j = 0; j < svgPaths.length; j++) {
-          svgPaths[j].style.fill = 'white'; // Weiße SVG-Farbe
-        }
-      }
-    } else if (priority === 'medium') {
-      selectedElement.style.backgroundColor = '#FFA800'; // Orange
-      selectedElement.style.color = 'white'; // Weiße Textfarbe
-      let svgPaths = selectedElement.querySelectorAll('svg path');
-      if (svgPaths) {
-        for (let j = 0; j < svgPaths.length; j++) {
-          svgPaths[j].style.fill = 'white'; // Weiße SVG-Farbe
-        }
-      }
-    } else if (priority === 'low') {
-      selectedElement.style.backgroundColor = '#7AE229'; // Grün
-      selectedElement.style.color = 'white'; // Weiße Textfarbe
-      let svgPaths = selectedElement.querySelectorAll('svg path');
-      if (svgPaths) {
-        for (let j = 0; j < svgPaths.length; j++) {
-          svgPaths[j].style.fill = 'white'; // Weiße SVG-Farbe
-        }
-      }
-    }
+  if (priority === 'urgent') {
+    setPriorityStyles(selectedElement, '#FF3D00', 'white', 'white');
+  } else if (priority === 'medium') {
+    setPriorityStyles(selectedElement, '#FFA800', 'white', 'white');
+  } else if (priority === 'low') {
+    setPriorityStyles(selectedElement, '#7AE229', 'white', 'white');
   }
 }
 
@@ -65,3 +58,4 @@ window.onload = function () {
     }
   }
 };
+
