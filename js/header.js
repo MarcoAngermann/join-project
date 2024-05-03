6c4b3a8dd0ee424bab52f806218d9035274a8b45
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-05-03 16:05:05 +0200

    test

--- js/header.js
+++ js/header.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-/**
- * Toggles the visibility of the dropdown content in the header.
- *
- */
-function headerDropdown() {
-  document.getElementById('dropdownContent').classList.toggle('show');
-}
-
-/**
- * Closes the dropdown menu if the click event occurs outside of the user emblem.
- *
- * @param {Event} event - The click event object.
- */
-window.onclick = function (event) {
-  if (!event.target.matches('.useremblem')) {
-    let dropdowns = document.getElementsByClassName('dropdownContent');
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-=======
 function showMenu() {
   document.getElementById('dropdownContent').classList.toggle('show');
 }
@@ -29,7 +8,6 @@ window.onclick = function (event) {
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
->>>>>>> 419af5b5ce5b26fcc83ab4e770f1f2d4263be2ed
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
