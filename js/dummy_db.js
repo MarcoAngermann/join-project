ed5dcf9738a7f780de93341e58d4a992fc29abf2
MónicaMorales <77685820+HTK-MM@users.noreply.github.com>
2024-05-06 00:25:06 +0200

    contact Details

---
+++ js/dummy_db.js
@@ -0,0 +1,47 @@
+let contacts = [
+    {
+        id: 1,
+        name: "Ackermann Carl Anton",
+        email: "carl_ackermann@gmx.de",
+        phone: "015498753529",
+        emblem: "AC",
+        color: "#FF7A00",
+    },
+    {
+        id: 2,
+        name: "Ahlers Johann August",
+        email: "johann8989@gmx.de",
+        phone: "016998639293",
+        emblem: "AJ",
+        color: "#FF4646",
+    },
+    {
+        id: 3,
+        name: "Beckmann Antje",
+        email: "beckmann.antje@yahoo.com",
+        phone: "018569875352",
+        emblem: "BA",
+        color: "#FFA35E",
+    },
+    {
+        id: 4,
+        name: "Mueller Susanne",
+        email: "sussi_mueller@gmx.de",
+        phone: "014569986987",
+        emblem: "MS",
+        color: "#C3FF2B"
+    },
+    {
+        id: 5,
+        name: "Wolfhope Theodor",
+        email: "theoHope1834@gmx.de",
+        phone: "014768932145",
+        emblem: "WT",
+        color: "#FF7A00"
+    }
+]
+
+
+let colors = ["#FF7A00", "#FF5EB3", "#6E52FF", "#9327FF", "#00BEE8", "#1FD7C1", "#FF745E",
+    "#FFA35E", "#FC71FF", "#FFC701", "#0038FF", "#C3FF2B", "#FFE62B", "#FF4646", "#FFBB2B"]
+
