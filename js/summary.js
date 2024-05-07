53b8323a7138365657dde97c6d666d7ef15774e7
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-05-07 08:07:58 +0200

    javascript greet funktion

---
+++ js/summary.js
@@ -0,0 +1,18 @@
+function getGreeting() {
+    let time = new Date().getHours();
+    let greeting;
+
+    if (time < 12) {
+        greeting = "Good Morning,";
+    } else if (time < 18) {
+        greeting = "Good Day,";
+    } else {
+        greeting = "Good Evening,";
+    }
+
+    return greeting;
+}
+
+function displayGreeting() {
+    document.getElementById("greetText").innerHTML = getGreeting();
+}
