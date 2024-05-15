f8fc54f86e0c081fcf5c5ee2c2058ce0c50dab49
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-05-15 13:07:04 +0200

    update addTask

--- js/add_task.js
+++ js/add_task.js
@@ -71,23 +71,8 @@ window.onload = function () {
   }
 };
 
-// let = checkedPrio;
-
-// let createTask = [
-//   {
-//   "title": title.value,
-//   "description": description.value,
-//   "assignees": [],
-//   "date": date.value,
-//   "priority": checkedPrio,
-//   "category": category.value,
-//   "subtask": [],
-// }
-// ];
-
 function renderAssignees() {
   let assignee = document.getElementById('assignees');
-
   for (let i = 0; i < contacts.length; i++) {
     const contact = contacts[i];
     assignee.innerHTML += /*html*/ `
@@ -100,13 +85,8 @@ function renderAssignees() {
             <input type="checkbox" id="checkbox${i}">          
         </li>
         </label>
-       `;
+      `;
   }
-}
-
-function toggleAssignees() {
-  let assigneesList = document.getElementById('assignees');
-  assigneesList.classList.toggle('show');
 }
 
 function renderCategorys() {
@@ -121,19 +101,26 @@ function renderCategorys() {
                     </div>
                 </span>
             </li>
-           `;
+        `;
   }
 }
 
-function toggleCategorys() {
-  let taskList = document.getElementById('tasks');
-  taskList.classList.toggle('show');
+function showCategories() {
+  if (document.getElementById('tasks').classList.contains('show')) {
+    document.getElementById('tasks').classList.remove('show');
+    document.getElementById('arrowDownCategory').style.display = 'block';
+    document.getElementById('arrowUpCategory').style.display = 'none';
+  } else {
+    document.getElementById('tasks').classList.add('show');
+    document.getElementById('arrowDownCategory').style.display = 'none';
+    document.getElementById('arrowUpCategory').style.display = 'block';
+  }
 }
 
 function selectCategory(index) {
   let selectedCategory = categorys[index];
   document.getElementById('selectedCategory').innerText = selectedCategory;
-  toggleCategorys(); // Hide the category list after selection
+  showCategories(); // Hide the category list after selection
 }
 
 function restrictPastDate() {
@@ -142,21 +129,19 @@ function restrictPastDate() {
   dateInput.setAttribute('min', today);
 }
 
-
 //Mónica New Funktion
-
 
 function renderEmblemAssignees(emblem, color) {
   let assignesEmblem = document.getElementById('assignesEmblem');
   assignesEmblem.innerHTML += `
     <div class="emblem" style="background-color: ${contact['color']}" id="${contact['id']}">
       ${contact['emblem']}
-    </div>  `
+    </div>  `;
 }
 
 function showAssigneesEmblem() {
   let assignesEmblem = document.getElementById('assignesEmblem');
-  assignesEmblem.innerHTML = "";
+  assignesEmblem.innerHTML = '';
   for (let i = 0; i < contacts.length; i++) {
     contact = contacts[i];
     let checkedContact = document.getElementById(`checkbox${i}`);
@@ -165,4 +150,153 @@ function showAssigneesEmblem() {
     }
   }
   document.getElementById('assignees').classList.toggle('close');
+}
+
+function showAssignees() {
+  if (document.getElementById('assignees').classList.contains('show')) {
+    showAssigneesEmblem();
+    document.getElementById('assignees').classList.remove('show');
+    document.getElementById('arrowDownAssignee').style.display = 'block';
+    document.getElementById('arrowUpAssignee').style.display = 'none';
+  } else {
+    document.getElementById('assignees').classList.add('show');
+    document.getElementById('arrowDownAssignee').style.display = 'none';
+    document.getElementById('arrowUpAssignee').style.display = 'block';
+  }
+}
+
+// Max Subtaks Functions
+function changeButtonsSubtask() {
+  document.getElementById('subtask-right-regular').classList.add('dnone');
+  document.getElementById('subtask-right-add').classList.remove('dnone');
+}
+
+function removeSubtask() {
+  subtask = document.getElementById('subtaskInput');
+  subtask.value = '';
+  document.getElementById('subtask-right-regular').classList.remove('dnone');
+  document.getElementById('subtask-right-add').classList.add('dnone');
+}
+
+function addSubtask() {
+  let input = document.getElementById('subtaskInput').value;
+  // Überprüfe, ob bereits 5 Subtasks vorhanden sind
+  if (subtaskList.length < 5) {
+    subtaskList.push(input);
+    renderSubtask();
+    document.getElementById('subtaskInput').value = '';
+    removeSubtask();
+  } else {
+    // Wenn bereits 5 Subtasks vorhanden sind, gib eine Warnung aus oder führe andere geeignete Aktionen durch
+    alert('Maximal 5 Subtasks sind erlaubt.');
+  }
+}
+
+function deleteSubtask(i) {
+  subtaskList.splice(i, 1);
+  renderSubtask();
+}
+
+function renderSubtask() {
+  let subtask = document.getElementById('subtask');
+  subtask.innerHTML = '';
+  for (let i = 0; i < Math.min(subtaskList.length, 5); i++) {
+    subtask.innerHTML += renderSubtaskHTML(i);
+  }
+}
+
+function renderSubtaskHTML(i) {
+  return /*html*/ `
+    <div class="subtaskList" id="mainSubtask-container${i}">
+            <input
+              readonly
+              type="text"
+              id="subtaskList${i}"
+              value="${subtaskList[i]}"
+              />
+                <div class="edit-images" id="edit-images${i}">
+                  <img onclick="editSubtask(${i})" id="editSubtask${i}" src="../assets/icons/edit_contacts_icon.svg" alt="">
+                  <div class="edit-seperator"></div>
+                  <img onclick="deleteSubtask(${i})" id="deleteSubtask${i}" src="../assets/icons/delete_contact_icon.svg" alt="">
+                </div>
+            </div>
+        </div>
+  `;
+}
+
+function editSubtask(i) {
+  document.getElementById(`subtaskList${i}`).readOnly = false;
+  edit = document.getElementById(`edit-images${i}`);
+  edit.innerHTML = editSubtaskHTML(i);
+  document
+    .getElementById(`mainSubtask-container${i}`)
+    .classList.remove('subtaskList');
+  document
+    .getElementById(`mainSubtask-container${i}`)
+    .classList.add('editsubtaskList');
+}
+
+function checkSubtask(i) {
+  document.getElementById(`subtaskList${i}`).readOnly = true;
+  edit = document.getElementById(`edit-images${i}`);
+  edit.innerHTML = checkSubtaskHTML(i);
+  document
+    .getElementById(`mainSubtask-container${i}`)
+    .classList.add('subtaskList');
+  document
+    .getElementById(`mainSubtask-container${i}`)
+    .classList.remove('editsubtaskList');
+}
+
+function editSubtaskHTML(i) {
+  return /*html*/ `
+    <img onclick="deleteSubtask(${i})" id="deleteSubtask${i}" src="../assets/icons/delete_contact_icon.svg" alt="">
+    <div class="edit-seperator"></div>
+    <img  onclick="checkSubtask(${i})" id="checkSubtask${i}" src="../assets/icons/check.svg" alt="">
+  `;
+}
+function checkSubtaskHTML(i) {
+  return /*html*/ `
+    <img onclick="editSubtask(${i})" id="editSubtask${i}" src="../assets/icons/edit_contacts_icon.svg" alt="">
+    <div class="edit-seperator"></div>
+    <img onclick="deleteSubtask(${i})" id="deleteSubtask${i}" src="../assets/icons/delete_contact_icon.svg" alt="">
+  `;
+}
+
+function getSelectedPrio() {
+  let urgentBtn = document.getElementById('urgentPrio');
+  let lowprioBtn = document.getElementById('lowPrio');
+  if (urgentBtn.classList.contains('selected')) {
+    return 'urgent';
+  } else if (lowprioBtn.classList.contains('selected')) {
+    return 'low';
+  } else {
+    return 'medium';
+  }
+}
+
+function getAssigneedContact() {
+  let assignesEmblem = document.getElementById('assignesEmblem');
+  let divs = assignesEmblem.getElementsByTagName('div');
+  let idsList = [];
+  for (let i = 0; i < divs.length; i++) {
+    idsList.push(divs[i].id);
+  }
+  return idsList;
+}
+
+function createNewTask(event) {
+  event.preventDefault();
+  let task = tasks[0];
+  task = {
+    title: document.getElementById('title').value,
+    description: document.getElementById('description').value,
+    assigneeIds: getAssigneedContact(),
+    date: document.getElementById('date').value,
+    priority: getSelectedPrio(),
+    category: document.getElementById('selectedCategory').value,
+    subtask: subtaskList,
+    status: 'To do',
+  };
+  tasks.push(task);
 }
